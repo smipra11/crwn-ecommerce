3a3622f396caea3931dc276c6952e00fa3937e84
Smita Deshpande <smitadeshpande@Smitas-MacBook-Pro.local>
2021-01-20 17:05:16 -0500

    added files for home page

---
+++ src/homepage.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import "./homepage.scss"
+
+ const Homepage  = () =>{
+return (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                   <h2 className="title">
+                     hat
+                   </h2>
+                   <span className="subtitle">shop now</span>
+
+                </div>
+
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                   <h2 className="title">
+                     Scarf
+                   </h2>
+                   <span className="subtitle">shop now</span>
+
+                </div>
+
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                   <h2 className="title">
+                      Jacket
+                   </h2>
+                   <span className="subtitle">shop now</span>
+
+                </div>
+
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                   <h2 className="title">
+                     cowboy hat
+                   </h2>
+                   <span className="subtitle">shop now</span>
+
+                </div>
+
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                   <h2 className="title">
+                     bennie
+                   </h2>
+                   <span className="subtitle">shop now</span>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+)
+
+}
+
+export default Homepage
