23e07f50091f02759a4a46999f2ad869e440abe4
Smita Deshpande <smitadeshpande@Smitas-MacBook-Pro.local>
2021-01-25 10:18:46 -0500

    updating collectionPreview and collectItem components

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import './App.css';
 import Homepage from "./page/homepage/homepage"
 import {Route,Switch} from 'react-router-dom'
 
-const Hatspage = () =>{
-  return(
-    <div>
-      <h1>Hat Page</h1>
-    </div>
-  )
-}
+import ShopPage from "./page/shop/shop"
+
+
 
 function App() {
   return (
     <div className="App">
       <Switch>
    <Route exact path= "/" component={Homepage} />
-   <Route path="/hats" component={Hatspage} />
+   <Route path="/shop" component={ShopPage} />
    </Switch>
      
     </div>
---
+++ src/components/preview-collection/preview-collection.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./preview-collection.scss"
+import CollectionItem from "../collection-item/collection-item"
+
+const CollectPreview = ({title,items}) =>{
+    return(
+        <div className="collection-preview">
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <div className= "preview">
+                {items.filter((item,idx) =>idx <4)
+                
+               . map((item) =>(
+                    //<div key={item.id}>{item.name} </div>
+                    <CollectionItem  key= {item.id}name={item.name} imageUrl={item.imageUrl} price={item.price}/>
+                ))}
+            </div>
+
+        </div>
+    )
+}
+
+export default CollectPreview
