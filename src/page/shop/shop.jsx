2e6da2d94b80fcecfc548691a6fbc271a17f44ea
Smita Deshpande <smitadeshpande@Smitas-MacBook-Pro.local>
2021-02-24 13:06:04 -0500

    adding stripe payment

--- src/page/shop/shop.jsx
+++ src/page/shop/shop.jsx
@@ -5,14 +5,19 @@ import React from 'react'
 
 import CollectPreview from "../../components/preview-collection/preview-collection"
 import CollectionOverview from "../../components/collections-overview/collection-overview"
+import {Route } from 'react-router-dom'
+import Collection from "../Collection/collection"
 
 
-const  ShopPage = () =>{
+const  ShopPage = ({match}) =>{
 
     
         return(
            <div className='shop-page'>
-               <CollectionOverview />
+               <Route exact path ={`${match.path}`} component ={CollectionOverview}/>
+               <Route path={`${match.path}/:collectionId`} component={Collection} />
+              
+              
             </div>
         )
     
